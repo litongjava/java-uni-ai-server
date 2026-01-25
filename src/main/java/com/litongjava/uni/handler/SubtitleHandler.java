e9e923d7a61a517312231c1d0629db78433b9f14
litongjava <litongjava001@gmail.com>
2026-01-25 22:23:54 +0800

    update upload file

--- src/main/java/com/litongjava/uni/handler/SubtitleHandler.java
+++ src/main/java/com/litongjava/uni/handler/SubtitleHandler.java
@@ -2,10 +2,10 @@ package com.litongjava.uni.handler;
 
 import com.litongjava.jfinal.aop.Aop;
 import com.litongjava.model.body.RespBodyVo;
+import com.litongjava.model.upload.UploadFile;
 import com.litongjava.tio.boot.http.TioRequestContext;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpResponse;
-import com.litongjava.tio.http.common.UploadFile;
 import com.litongjava.tio.http.server.handler.HttpRequestHandler;
 import com.litongjava.uni.services.UniSubtitleService;
 
